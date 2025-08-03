9896e3fb617ae0ac920e14551645565cfc3b8fd6
SpeedHub79 <romeovelasquezcastillo@gmail.com>
2025-08-03 11:35:15 +0800

    Update Speed%Hub%X.lua

--- Speed%Hub%X.lua
+++ Speed%Hub%X.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
 loadstring(game:HttpGet("https://pastefy.app/UyCoiCsm/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/T9sZdtAr/raw"))()
